807141826a3b0332b82e529ad7fcb18aee8a1467
Tzippy Itzkovich <tzippy6160@gmail.com>
2023-07-04 20:06:38 +0300

    add components and style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// // App.js
 
-function App() {
+import React, { useState } from 'react';
+import LotteryCard from './Components/LotteryCard/LotteryCard.js';
+import './App.css'; // Import the CSS file for the App component
+import log5071go from './g5071.svg';
+
+const lotteryData = [
+  {
+    id: 1,
+    lotteryName: 'Lottery A',
+    jackpot: '1,000,000',
+    drawTime: '8:00 PM',
+    resultDateTime: '2023-07-04 20:00:00',
+    resultNumbers: [5, 10, 15, 20, 25],
+    logoSrc: log5071go,
+  },
+  {
+    id: 2,
+    lotteryName: 'Lottery B',
+    jackpot: '2,000,000',
+    drawTime: '10:30 PM',
+    resultDateTime: '2023-07-04 22:30:00',
+    resultNumbers: [3, 7, 12, 18, 30],
+    logoSrc: log5071go,
+  },
+  // Add more lottery data objects as needed
+];
+
+
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const filteredLotteries = lotteryData.filter((lottery) => {
+    return lottery.lotteryName.toLowerCase().includes(searchText.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <input
+        type="text"
+        placeholder="Search by lottery name..."
+        value={searchText}
+        onChange={handleSearchChange}
+      />
+      <div className="lottery-cards-container">
+        {filteredLotteries.map((lottery) => (
+          <LotteryCard key={lottery.id} lottery={lottery} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
---
+++ src/Components/LotteryCard/LotteryCard.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './LotteryCard.css';
+
+const LotteryCard = ({ lottery }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleLinkClick = (e) => {
+    e.stopPropagation();
+  };
+
+  const renderBlueSide = () => {
+    return (
+      <div className="blue-side">
+        <h3 className="jackpot">{lottery.jackpot}</h3>
+        <p className="draw-time">Draw Time: {lottery.drawTime}</p>
+        <img className="lottery-logo" src={lottery.logoSrc} alt={lottery.lotteryName} />
+        <p className="lottery-name">{lottery.lotteryName}</p>
+        <button className="card-link" onClick={handleLinkClick}>Link to another page</button>
+        <button className="flip-btn" onClick={handleFlip}>Result</button>
+      </div>
+    );
+  };
+
+  const renderResultSide = () => {
+    return (
+      <div className="result-side">
+        <p className="result-datetime">Static Date/Time: {lottery.resultDateTime}</p>
+        <div className="result-numbers">
+          {lottery.resultNumbers.slice(0, 8).map((number, index) => (
+            <span key={index} className="result-ball">{number}</span>
+          ))}
+        </div>
+        <button className="card-link" onClick={handleLinkClick}>Link to another page</button>
+        <button className="flip-btn" onClick={handleFlip}>Back</button>
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className={`lottery-card ${isFlipped ? 'flipped' : ''}`}
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          {renderBlueSide()}
+        </div>
+        <div className="card-back">
+          {renderResultSide()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LotteryCard;
